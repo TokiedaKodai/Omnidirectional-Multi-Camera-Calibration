c04bbe29fd4af1d57965d34e31617ffc24ec2161
Tokieda Kodai <tokieda.kodai.281@s.kyushu-u.ac.jp>
2021-04-14 17:37:38 +0900

    delete depth

--- capture_realsense.py
+++ capture_realsense.py
@@ -34,7 +34,7 @@ is_camera_3 = False
 pipeline_1 = rs.pipeline()
 config_1 = rs.config()
 config_1.enable_device(cf.CAMERA_1)
-config_1.enable_stream(rs.stream.depth, cf.CAPTURE_WIDTH, cf.CAPTURE_HEIGHT, rs.format.z16, cf.CAPTURE_FPS)
+# config_1.enable_stream(rs.stream.depth, cf.CAPTURE_WIDTH, cf.CAPTURE_HEIGHT, rs.format.z16, cf.CAPTURE_FPS)
 config_1.enable_stream(rs.stream.color, cf.CAPTURE_WIDTH, cf.CAPTURE_HEIGHT, rs.format.bgr8, cf.CAPTURE_FPS)
 # Camera 2
 pipeline_2 = rs.pipeline()
@@ -69,7 +69,7 @@ try:
         if is_camera_1:
             # Wait for a coherent pair of frames: depth and color
             frames_1 = pipeline_1.wait_for_frames()
-            depth_frame_1 = frames_1.get_depth_frame()
+            # depth_frame_1 = frames_1.get_depth_frame()
             color_frame_1 = frames_1.get_color_frame()
             if not depth_frame_1 or not color_frame_1:
                 continue
@@ -111,7 +111,8 @@ try:
         # Stack all images horizontally
         list_stack = []
         if is_camera_1:
-            list_stack.append(color_image_1, depth_colormap_1)
+            # list_stack.append(color_image_1, depth_colormap_1)
+            list_stack.append(color_image_1)
         if is_camera_2:
             list_stack.append(color_image_2, depth_colormap_2)
         if is_camera_2:
@@ -127,7 +128,7 @@ try:
         ch = cv2.waitKey(25)
         if ch == ord('1') and is_camera_1:
             cv2.imwrite(save_image.format(ch, idx1), color_image_1)
-            cv2.imwrite(save_depth.format(ch, idx1), depth_colormap_1)
+            # cv2.imwrite(save_depth.format(ch, idx1), depth_colormap_1)
             idx1 += 1
             print('Save camera-{}'.format(ch))
         elif ch == ord('2') and is_camera_2:
